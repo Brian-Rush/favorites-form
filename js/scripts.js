5607c99232ebf860c260434983f86db5510f5d4a
Brian Rush and Leah Sherrell <brian+leah@>
2017-05-01 11:48:00 -0700

    access specific element in array, then push them to new array, then display new array as a UL.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,34 @@
 $(document).ready(function() {
-  $("#favs").submit(function(event) {
-    event.preventDefault();
+  $("#favorites").submit(function(event) {
     var band = $("#band").val();
-    alert("asdlfkjs");
+    var cocktail = $("#cocktail").val();
+    var movie = $("#movie").val();
+    var book = $("#book").val();
+    var coffeePlace = $("#coffee").val();
+    // console.log(band);
+    // console.log(cocktail);
+    // console.log(movie);
+    // console.log(book);
+    // console.log(coffeePlace);
+
+    var favs = [band, cocktail, movie, book, coffeePlace];
+    console.log(favs);
+    var favs2 = [];
+    var first = favs[1];
+    var zeroth = favs[0];
+    var second = favs[2];
+    favs2.push(first, zeroth, second);
+
+    // $("#empty-ul").text(favs2);
+
+    $('#empty-ul li').each(function (index) {
+        $(this).text(favs2[index]);
+    });
+    // alert(favs);
+
+
+
+    event.preventDefault();
   });
 
 });
